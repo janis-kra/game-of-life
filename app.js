633f714eb9f3d96ff2626ea685b4a9f12c67ad24
Janis Krasemann <krasemann.janis@gmail.com>
2016-07-06 17:28:20 +0200

    Imlement an algorithm that determines if a cell lives or dies
    (has a bug somewhere)

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
 const state = mobx.observable({
   cells: [],
   size: {
-    x: 10,
-    y: 10
+    x: 5,
+    y: 5
   }
 });
+
+const getNeighborsForCoords = (row, col) => [
+  [row - 1, col - 1],
+  [row - 1, col],
+  [row - 1, col + 1],
+  [row, col - 1],
+  [row, col + 1],
+  [row + 1, col - 1],
+  [row + 1, col],
+  [row + 1, col + 1]
+].map((coords) => {
+  const x = coords[1] === -1 ? state.size.x - 1 :
+    coords[1] === state.size.x ? 0 : coords[1];
+  const y = coords[0] === -1 ? state.size.y - 1 :
+    coords[0] === state.size.y ? 0 : coords[0];
+  return state.cells[x][y];
+});
+
+const createCell = (cell, col, row) => {
+  let age;
+  const neighbors = getNeighborsForCoords(row, col);
+  const aliveNeighbors = neighbors.reduce((aliveCells, c) => {
+    return aliveCells + (c.age === 0 ? 0 : 1);
+  }, 0);
+  if (aliveNeighbors > 2 ||
+    aliveNeighbors === 2 && cell.age > 0) {
+    age = cell.age + 1; // springs alive or gets older
+  } else {
+    age = 0;
+  }
+  return {
+    ...cell,
+    age: age
+  };
+};
+
+const updateCells = () => {
+  const cells = state.cells.map((cellColumn, colIndex) =>
+    cellColumn.map((cell, index) => createCell(cell, colIndex, index))
+  );
+  state.cells = cells;
+};
 
 const getClassForCell = (age) => {
   if (age <= 0) {
@@ -18,7 +60,7 @@ const getClassForCell = (age) => {
 
 const rollAge = () => {
   const roll = Math.random();
-  if (roll < 0.8) {
+  if (roll < 0.85) {
     return 0;
   } else {
     return 1;
@@ -51,12 +93,13 @@ const Controls = () => (
 );
 
 const App = () => {
-  for (let i = 0; i < state.size.x; i++) {
-    state.cells.push([]);
-    for (let j = 0; j < state.size.y; j++) {
-      state.cells[i].push({ age: rollAge() });
+  for (let i = 0; i < state.size.x; i++) { // cols
+    state.cells.push([]); // push new column
+    for (let j = 0; j < state.size.y; j++) { // rows
+      state.cells[i].push({ age: rollAge() }); // push new cell at row j and column i
     }
   }
+  updateCells();
   return (
     <div>
       <h1>Game Of Life</h1>
