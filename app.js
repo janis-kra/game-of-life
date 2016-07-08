869c41d6b8fe130d742245d85bf3832b7871a845
Janis Krasemann <krasemann.janis@gmail.com>
2016-07-08 19:03:55 +0200

    Add missing controls and fix some styles

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const state = mobx.observable({
   cells: [],
+  generations: 0,
   size: {
-    x: 5,
-    y: 5
+    x: 50,
+    y: 50
   },
-  running: false,
-  speed: 1000 // speed in ms
+  running: true,
+  speed: 100 // speed in ms
 });
 
 const getNeighborsForCoords = (col, row) => [
@@ -46,6 +47,7 @@ const updateCell = (cell, col, row) => {
 };
 
 const updateCells = () => {
+  state.generations = state.generations + 1;
   const cells = state.cells.map((cellColumn, colIndex) =>
     cellColumn.map((cell, index) => updateCell(cell, colIndex, index))
   );
@@ -64,7 +66,7 @@ const getClassForCell = (age) => {
 
 const rollAge = () => {
   const roll = Math.random();
-  if (roll < 0.85) {
+  if (roll < 0.9) {
     return 0;
   } else {
     return 1;
@@ -73,32 +75,42 @@ const rollAge = () => {
 
 const restartCells = () => {
   state.running = false;
+  state.generations = 0;
   state.cells = [];
   for (let i = 0; i < state.size.x; i++) { // cols
     state.cells.push([]); // push new column
     for (let j = 0; j < state.size.y; j++) { // rows
-      state.cells[i].push({ age: rollAge() }); // push new cell at row j and column i
+      state.cells[i].push({ age: rollAge(), x: i, y: j }); // push new cell at row j and column i
     }
   }
 };
 
 const start = () => {
-  if (!state.running) { // only start if the game is not already running
-    state.running = true;
-    setInterval(() => {
-      if (state.running) {
-        updateCells();
-      }
-    }, state.speed);
-  }
+  setInterval(() => {
+    if (state.running) {
+      updateCells();
+    }
+  }, state.speed);
 };
 
-const stop = () => {
+const clear = () => {
   state.running = false;
+  state.cells = [];
+  state.generations = 0;
 };
 
-const Cell = ({ age }) => (
-  <div className={ 'cell ' + getClassForCell(age) } />
+const toggleRunning = () => {
+  state.running = !state.running;
+};
+
+const Cell = ({ age, x, y }) => (
+  <div className={ 'cell ' + getClassForCell(age) }
+       onClick={
+         () => {
+           const cellState = state.cells[x][y];
+           cellState.age = (!cellState.age) | 0;
+         }
+       }/>
 );
 
 /**
@@ -106,32 +118,41 @@ const Cell = ({ age }) => (
  */
 const Life = mobxReact.observer(() => (
   <div className='life'>
-    { state.cells.map((cellRow) => {
+    { state.cells.map((column) => {
       return (
-        <div className='cellRow'>
-          { cellRow.map((cell) => <Cell {...cell} />) }
+        <div className='column'>
+          { column.map((cell) => <Cell {...cell} />) }
         </div>
       );
     })}
   </div>
 ));
 
-const Controls = () => (
+const Controls = mobxReact.observer(() => (
   <div className='controls'>
-    <button onClick={ updateCells }>Step</button>
-    <button onClick={ start }>Start</button>
-    <button onClick={ stop }>Stop</button>
+    <div>Generation { state.generations }</div>
+    <button onClick={ toggleRunning }>Start / Stop</button>
     <button onClick={ restartCells }>Restart</button>
+    <button onClick={ clear }>Clear</button>
+  </div>
+));
+
+const Footer = () => (
+  <div className='footer'>
+    <h4>by <a href='https://github.com/janis-kra'>Janis Krasemann</a></h4>
   </div>
 );
 
 const App = () => {
   restartCells();
+  state.running = true;
+  start();
   return (
     <div>
       <h1>Game Of Life</h1>
       <Controls />
       <Life />
+      <Footer />
     </div>
   );
 };
